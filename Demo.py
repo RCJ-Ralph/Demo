6acc6cb2afe824d3bab801530f7722d15b8882ae
Jason Cai <cjz310260@gmail.com>
2026-02-18 20:29:14 +1000

    c2

--- Demo.py
+++ Demo.py
@@ -4,5 +4,7 @@ print("I am learning how to use GitHub for version control.")
 print("I can make changes to this file and commit them to GitHub.")
 print("This is a great way to collaborate with others on coding projects.")
 
+print("I can also create branches to work on new features without affecting the main codebase.")
+print("GitHub allows me to track changes and see the history of my code.")
 
 print("Thank you for reading this demo file!")
