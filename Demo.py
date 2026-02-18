1622fc1d46545f8790f11a97c1bb29a3b1649323
Jason Cai <cjz310260@gmail.com>
2026-02-18 20:23:31 +1000

    change 1

--- Demo.py
+++ Demo.py
@@ -1,7 +1,10 @@
 print("Hello GitHub!")
 print("This is a demo file for GitHub.")
+print("I am learning how to use GitHub for version control.")
 print("I can make changes to this file and commit them to GitHub.")
 print("This is a great way to collaborate with others on coding projects.")
 
 print("I can also create branches and merge them back to the main branch.")
+print("GitHub allows me to track changes and see the history of my code.")
+
 print("GitHub is a powerful tool for developers and teams.")
